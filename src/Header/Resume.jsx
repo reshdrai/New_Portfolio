46f94a10e73fc56c9e220602670f3c4a2d263527
reshdrai <141801808+reshdrai@users.noreply.github.com>
2024-08-21 10:10:36 +0545

    Second commit

--- src/Header/Resume.jsx
+++ src/Header/Resume.jsx
@@ -11,8 +11,64 @@ function Resume() {
         </div>
         <div className="resume-container" >
             <div className="row">
-        <div className="resume-left col-lg-5 col-sm-12">hello</div>
-        <div className="resume-right col-lg-5 col-sm-12">hi</div>
+        <div className="resume-left col-lg-6 col-sm-12">
+      
+        <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" fill="rgba(255, 255, 255, 0.1)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">  <line class="st0" x1="20" y1="24" x2="20" y2="29"></line> <path class="st0" d="M18.8,11.2c-1.1,0.2-2,0-2.4-0.7c-0.4-0.7-0.1-1.7,0.6-2.5c-0.9-0.9-1.9-1.5-3.1-1.8c-0.3,1-1,1.8-1.8,1.8 s-1.5-0.7-1.8-1.8C9,6.6,7.9,7.2,7.1,8c0.7,0.8,1,1.8,0.6,2.5c-0.4,0.7-1.4,0.9-2.4,0.7C5.1,11.8,5,12.4,5,13s0.1,1.2,0.2,1.8 c1.1-0.2,2,0,2.4,0.7c0.4,0.7,0.1,1.7-0.6,2.5c0.9,0.9,1.9,1.5,3.1,1.8c0.3-1,1-1.8,1.8-1.8s1.5,0.7,1.8,1.8 c1.2-0.3,2.3-0.9,3.1-1.8c-0.7-0.8-1-1.8-0.6-2.5c0.4-0.7,1.4-0.9,2.4-0.7c0.2-0.6,0.2-1.2,0.2-1.8S18.9,11.8,18.8,11.2z"></path> <g> <path d="M12,14c-0.1,0-0.3,0-0.4-0.1c-0.1-0.1-0.2-0.1-0.3-0.2c-0.1-0.1-0.2-0.2-0.2-0.3c0-0.1-0.1-0.3-0.1-0.4 c0-0.1,0-0.3,0.1-0.4c0.1-0.1,0.1-0.2,0.2-0.3c0.4-0.4,1-0.4,1.4,0c0.1,0.1,0.2,0.2,0.2,0.3c0,0.1,0.1,0.3,0.1,0.4 c0,0.1,0,0.3-0.1,0.4c-0.1,0.1-0.1,0.2-0.2,0.3C12.5,13.9,12.3,14,12,14z"></path> </g> <line class="st0" x1="10" y1="19.7" x2="10" y2="27"></line> <path class="st0" d="M19,24h4c1.1,0,2-0.9,2-2v-4h3l-3-6c0-5-4-9-9-9c-3,0-5.7,1.5-7.3,3.8"></path> </g></svg><span style={{fontSize : '42px'}}>Experience</span>
+         
+           
+          <div className="experience-container" style={{borderLeft : '1px solid #232935', padding : "0 0 0 2.5rem",margin : '2rem 0 0 0' }}>
+            <div className="experience-cont" style={{
+          padding :' 0 0 2rem 0'
+            }}>
+            <h2 style={{margin : '0 0 12px 0', color :"#fec544"}}>React developer</h2>
+            <h6 style={{margin : '0 0 1rem 0', color : '#a9adb8'}}>May 2023 - Present</h6>
+          <h5 style={{margin :" 0 0 1.24em 0"}}>Independent Developer</h5>
+          <p style={{color : '#a9adb8'}}>
+          With a year of experience as a React developer, I specialize in building dynamic, responsive web applications, focusing on clean code and seamless user interactions.</p>
+          </div>
+          </div>
+
+          <div className="experience-container" style={{borderLeft : '1px solid #232935', padding : "0 0 0 2.5rem",margin : '0 0 0 0' }}>
+            <div className="experience-cont" style={{
+          padding :' 0 0 0 0'
+            }}>
+            <h2 style={{margin : '0 0 12px 0', color :"#fec544"}}>Java developer</h2>
+            <h6 style={{margin : '0 0 1rem 0', color : '#a9adb8'}}>Sept 2023 - Present</h6>
+          <h5 style={{margin :" 0 0 1.24em 0"}}>Independent Developer</h5>
+          <p style={{color : '#a9adb8'}}>
+          With 1 year of experience in Java development , I specialize in back-end development using the Spring Boot framework, focusing on building efficient and scalable server-side applications.</p>
+          </div>
+          </div>
+        </div>
+        <div className="resume-right col-lg-6 col-sm-12">
+        <svg fill="rgba(255, 255, 255, 0.1)" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M197.769 791.767l60.672-286.853c2.341-11.066-4.733-21.934-15.799-24.275s-21.934 4.733-24.275 15.799l-60.672 286.853c-2.341 11.066 4.733 21.934 15.799 24.275s21.934-4.733 24.275-15.799zm571.063-286.786l61.778 287.068c2.38 11.058 13.273 18.093 24.33 15.713s18.093-13.273 15.713-24.33l-61.778-287.068c-2.38-11.058-13.273-18.093-24.33-15.713s-18.093 13.273-15.713 24.33z"></path><path d="M967.45 386.902L535.9 208.126c-10.609-4.399-30.569-4.442-41.207-.088L57.821 386.901l436.881 178.857c10.624 4.355 30.583 4.313 41.207-.085L967.45 386.901zM551.583 603.516c-20.609 8.533-51.787 8.599-72.409.145L24.437 417.494c-32.587-13.359-32.587-47.847.009-61.188l454.73-186.174c20.641-8.448 51.818-8.382 72.407.156l448.836 185.936c32.466 13.442 32.466 47.913.004 61.354l-448.84 185.938zm288.673 166.569c-98 57.565-209.669 88.356-325.888 88.356-116.363 0-228.162-30.866-326.246-88.564-9.749-5.735-22.301-2.481-28.036 7.268s-2.481 22.301 7.268 28.036c104.336 61.377 223.297 94.22 347.014 94.22 123.564 0 242.386-32.763 346.634-93.998 9.753-5.729 13.015-18.279 7.286-28.032s-18.279-13.015-28.032-7.286z"></path><path d="M983.919 383.052v296.233c0 11.311 9.169 20.48 20.48 20.48s20.48-9.169 20.48-20.48V383.052c0-11.311-9.169-20.48-20.48-20.48s-20.48 9.169-20.48 20.48z"></path></g></svg><span style={{fontSize : '42px'}}>Education</span>
+         
+           
+         <div className="experience-container" style={{borderLeft : '1px solid #232935', padding : "0 0 0 2.5rem",margin : '2rem 0 0 0' }}>
+           <div className="experience-cont" style={{
+         padding :' 0 0 2rem 0'
+           }}>
+           <h2 style={{margin : '0 0 12px 0', color :"#fec544"}}>
+           BSc (Hons) in Software Engineeringr</h2>
+           <h6 style={{margin : '0 0 1rem 0', color : '#a9adb8'}}>Sept 2023 - Present</h6>
+         <h5 style={{margin :" 0 0 1.24em 0"}}>PCPS College</h5>
+         <p style={{color : '#a9adb8'}}>
+         I'm currently pursuing a BSc (Hons) in Software Engineering at PCPS College, affiliated with the University of Bedfordshire.</p>
+         </div>
+         </div>
+
+         <div className="experience-container" style={{borderLeft : '1px solid #232935', padding : "0 0 0 2.5rem",margin : '0 0 0 0' }}>
+           <div className="experience-cont" style={{
+         padding :' 0 0 2.6rem 0'
+           }}>
+           <h2 style={{margin : '0 0 12px 0', color :"#fec544"}}>High School</h2>
+           <h6 style={{margin : '0 0 1rem 0', color : '#a9adb8'}}>May 2021 - 2023</h6>
+         <h5 style={{margin :" 0 0 1.24em 0"}}>Pinnacle Academy</h5>
+         <p style={{color : '#a9adb8', }}>
+         I completed my high schooling at Pinnacle Academy.</p>
+         </div>
+         </div>
+        </div>
         </div>
         </div>
         </div>
